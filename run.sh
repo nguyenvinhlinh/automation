338e6cc18eeac36f42255d0997c872fcb78d594d
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-07-24 22:04:35 +0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+sudo yum install git -y;
+sudo yum install emacs -y;
+mkdir ~/Software;
+git clone git@github.com:retroj/conkeror.git ~/Software/Conkeror;
+git clone git@github.com:ziahamza/webui-aria2.git ~/Software/webui-aria2;
+mkdir ~/Projects;
+git clone git@github.com:nguyenvinhlinh/.emacs.d.git ~/.emacs.d;
+git clone git@github.com:nguyenvinhlinh/.conkerorrc.git ~/..conkerorrc;
+
+
