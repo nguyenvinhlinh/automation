9c6ef666f62849e73f8a9831d58160c4093c96cf
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2015-12-10 15:52:56 +0700

    finished

--- run.sh
+++ run.sh
@@ -1,23 +1,35 @@
-sudo dnf install emacs -y;
-mkdir ~/Software;
-git clone git@github.com:retroj/conkeror.git ~/Software/Conkeror;
-git clone git@github.com:ziahamza/webui-aria2.git ~/Software/webui-aria2;
-mkdir ~/Projects;
-git clone git@github.com:nguyenvinhlinh/.emacs.d.git ~/.emacs.d;
-git clone git@github.com:nguyenvinhlinh/.conkerorrc.git ~/.conkerorrc;
-git clone git@github.com:nguyenvinhlinh/nguyenvinhlinh.github.io.git ~/Projects/nguyenvinhlinh.github.io;
-git clone -b intergrate-aria2c git@github.com:nguyenvinhlinh/conkeror.git ~/Software/Conkeror;
-cd ~/Software/Conkeror;
-git remote add origin-retroj git@github.com:retroj/conkeror.git;
-cd ~;
-# Install zsh and autojump
-sudo dnf install zsh -y;
-sudo dnf install autojump-zsh -y;
-#Install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+passpharse_ssh=""
+email_ssh=""
+passpharse_current_user=""
+
+### Install software
+softwares=("git" "emacs" "zsh" "autojump-zsh" "aria2" "xclip" "expect");
+for software  in "${softwares[@]}"
+do
+    echo "Installing "$software;
+    sudo dnf install $software -y;
+done
+
+### Make directory
+directories=("~/Software" "~/Projects")
+for dir in "${directory[@]}"
+do
+    mkdir $dir;
+done
+
+### Generate ssh pub/private key
+ssh-keygen -t rsa -b 4096 -C "${email_ssh}" -N "${passpharse_ssh}" -f ~/.ssh/rsa_key_file;
+
+### Install git repositories
+git clone https://github.com/ziahamza/webui-aria2.git ~/Software/webui-aria2;
+git clone https://github.com/nguyenvinhlinh/.emacs.d.git ~/.emacs.d;
+git clone https://github.com/nguyenvinhlinh/nguyenvinhlinh.github.io.git ~/Projects/nguyenvinhlinh.github.io;
+git clone -b intergrate-aria2c https://github.com/nguyenvinhlinh/conkeror.git ~/Software/Conkeror;
+git -C ~/Software/Conkeror remote add origin-retroj git@github.com:retroj/conkeror.git;
+git clone https://github.com/nguyenvinhlinh/.conkerorrc.git ~/.conkerorrc;
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh;
+
+### Fetch the zsh configuration file
 curl -o .zshrc https://raw.githubusercontent.com/nguyenvinhlinh/zshrc/master/.zshrc;
-#Change default shell
-chsh -s /usr/bin/zsh;
-#Install Aria2c
-sudo dnf install aria2 -y;
-sudo dnf update -y;
+### Change defaul shell from bash to zsh
+expect change_shell.exp "$passpharse_current_user";
